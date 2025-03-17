6f25d127347f4da166e50db61a7f6fc99b7b67f8
Łukasz <l.strekowski@gmail.com>
2025-03-17 20:42:40 +0100

    disable supabase

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -45,7 +45,7 @@ export default defineNuxtConfig({
     '@vueuse/nuxt',
     "@nuxt/eslint",
     // "@nuxtjs/dotenv",
-    "@nuxtjs/supabase",
+    // "@nuxtjs/supabase",
     [
       "@pinia/nuxt",
       {
