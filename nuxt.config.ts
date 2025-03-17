e2e8bd9a336bce0aa99fcd60a5faf29b0d1ef252
Łukasz <l.strekowski@gmail.com>
2025-03-17 17:55:01 +0100

    env

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -18,6 +18,11 @@ export default defineNuxtConfig({
     // supabaseUrl: "https://ipkenjukgvroconcxnnr.supabase.co",
     // supabaseAnonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imlwa2VuanVrZ3Zyb2NvbmN4bm5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDA5MzA4OTgsImV4cCI6MjAxNjUwNjg5OH0.y8qJk3r_6JnUjCgaY6xxzhBhI6LYozel5HzTz4DBVyA"
   },
+  supabase: {
+    url: process.env.SUPABASE_URL,
+    key: process.env.SUPABASE_KEY,
+    redirect: false,
+  },
   app: {
     head: {
       link: [{ rel: 'icon', type: 'image/png', href: './favicon.png' }]
